7afe394901fd75ed1a9deedfddccb0b2c02a4167
g-div <g-div@anche.no>
2014-04-07 01:55:27 +0200

    restructuring the OSM-import in order to use the Waterline-ORM provided by Sails

--- api/services/fetchData.js
+++ api/services/fetchData.js
@@ -1,58 +1,66 @@
 #!/usr/bin/env node
-
+'use strict';
 var request = require('request'),
+	_ = require('lodash'),
 	fs = require('fs'),
 	path = require('path'),
 	JSONStream = require('JSONStream'),
 	es = require('event-stream'),
 	writeStream = fs.createWriteStream(path.resolve(__dirname, '..', '..', 'data', 'data.json')),
-	pkg = require(path.resolve(__dirname, '..', '..', 'package.json')),
-	connection = 'mongodb://' + pkg.config.adapter.user + ':' + pkg.config.adapter.password + '@' + pkg.config.adapter.host + ':' + pkg.config.adapter.port + '/' + pkg.config.adapter.database,
-	MongoClient = require('mongodb').MongoClient,
-	mongodb = {};
-
-MongoClient.connect(connection, function(err, db) {
-	if (err) throw err;
-	mongodb = db;
-})
+	pkg = require(path.resolve(__dirname, '..', '..', 'package.json'));
 
 var baseURL = 'http://overpass-api.de/',
 	queryURL = 'api/interpreter?data=',
 	query = '<osm-script output="json"> <union> <query type="node"> <has-kv k="man_made" v="surveillance"/> <bbox-query s="' + pkg.config.bbox.lat1 + '" w="' + pkg.config.bbox.lon1 + '" n="' + pkg.config.bbox.lat2 + '" e="' + pkg.config.bbox.lon2 + '"/> </query> <query type="way"> <has-kv k="man_made" v="surveillance"/> <bbox-query s="' + pkg.config.bbox.lat1 + '" w="' + pkg.config.bbox.lon1 + '" n="' + pkg.config.bbox.lat2 + '" e="' + pkg.config.bbox.lon2 + '"/> </query> <query type="relation"> <has-kv k="man_made" v="surveillance"/> <bbox-query s="' + pkg.config.bbox.lat1 + '" w="' + pkg.config.bbox.lon1 + '" n="' + pkg.config.bbox.lat2 + '" e="' + pkg.config.bbox.lon2 + '"/> </query> </union> <print mode="body"/> <recurse type="down"/> <print mode="skeleton"/> </osm-script>';
 
-var writeData = function write(data) {
+
+var writeData = function(data) {
 	var lat = data.lat,
 		lng = data.lon,
-		isValidData = (typeof lat !== 'undefined' && lat !== '') && (typeof lng !== 'undefined' && lng != '');
+		isValidData = (typeof lat !== 'undefined' && lat !== '') &&
+					(typeof lng !== 'undefined' && lng !== '') &&
+					(typeof data.tags !== 'undefined');
 
 	if (isValidData) {
-		var location = [parseFloat(lat.toFixed(6)), parseFloat(lng.toFixed(6))];
+		var entry = {
+				location: [parseFloat(lat.toFixed(6)), parseFloat(lng.toFixed(6))],
+				name: data.tags.name,
+				description: data.tags.description,
+				url: data.tags.webcam,
+				osmID: data.id
+			};
 
-		mongodb.collection('cctv').update({_id: location.join('').replace(/\./g, '')},
-			{location: location, _id: location.join('').replace(/\./g, '')},
-			{upsert: true},
-			function(err, cctv) {
-				if (err) throw err;
+		entry = _.omit(entry, function(value) {
+			return value === undefined;
+	    });
+
+		Cctv.find({'osmID': entry.osmID}).done(function(err, cctv) {
+			if (err) {
+				return console.log(err);
+			} else if (cctv.length === 0) {
+				Cctv.create(entry, function(err, cctv) {
+				  if (err) return console.log(err);
+				});
+			}
 		});
 
 		this.emit('data', data);
 	}
-}
+};
 
 function tap() {
 	return es.through(writeData);
 }
 
 request({url: baseURL + queryURL + query})
-// parse interesting elements
-.pipe(JSONStream.parse("elements.*"))
-.pipe(tap())
-// write back to string
-.pipe(JSONStream.stringify(false))
-// write to the filesystem
-.pipe(writeStream);
+	// parse interesting elements
+	.pipe(JSONStream.parse('elements.*'))
+	.pipe(tap())
+	// write back to string
+	.pipe(JSONStream.stringify(false))
+	// write to the filesystem
+	.pipe(writeStream);
 
 writeStream.on('close', function(){
-	console.log("Saved !");
-	mongodb.close();
+	console.info('OSM data synced !');
 });
