03301439ba2db48afefd2a85944f29ab72f7f6d2
moklick <info@moritzklack.com>
2014-04-16 20:08:43 +0200

    change config details to match readme, delete beforeValidation method

--- api/services/LocationHelper.js
+++ api/services/LocationHelper.js
@@ -1,6 +1,7 @@
 module.exports = {
 
 	parseLocationString : function(locationString){
+		console.log(locationString);
 		var location = locationString.split(',');
 		return [ parseFloat(location[0]),parseFloat(location[1]) ];
 	}
