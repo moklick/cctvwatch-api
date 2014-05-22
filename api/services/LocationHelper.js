7128d7c03c757f31d7018275d93ae388851a9dd0
g-div <g-div@anche.no>
2014-05-22 02:42:12 +0200

    logging removed

--- api/services/LocationHelper.js
+++ api/services/LocationHelper.js
@@ -1,7 +1,6 @@
 module.exports = {
 
 	parseLocationString : function(locationString){
-		console.log(locationString);
 		var location = locationString.split(',');
 		return [ parseFloat(location[0]),parseFloat(location[1]) ];
 	}
