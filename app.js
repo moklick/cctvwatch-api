c0b627258c6638d5ede0ae1a358bb9f5a0e55333
g-div <g-div@anche.no>
2014-05-31 20:07:29 +0200

    sockets disabled

--- app.js
+++ app.js
@@ -1,2 +1,6 @@
 // Start sails and pass it command line arguments
-require('sails').lift(require('optimist').argv);
+require('sails').lift({
+  hooks: {
+    sockets: false,
+    pubsub: false
+  }});
