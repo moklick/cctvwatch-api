7bbc7742f3c51a594888775a5ff397385b01211d
g-div <g-div@anche.no>
2014-06-03 00:36:34 +0200

    more options added to the package.json

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 // Start sails and pass it command line arguments
+var path = require('path'),
+	config = require(path.resolve(__dirname, 'api', 'services', 'ConfigManager')).getConfig();
 require('sails').lift({
+  port: config.port,
+  prod: config.production,
   hooks: {
     sockets: false,
     pubsub: false
