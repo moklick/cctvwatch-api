34a47ac1a905fe409c16d7aada6fe59a9d630303
g-div <g-div@anche.no>
2014-06-09 19:58:02 +0200

    don't handle port number anymore

--- app.js
+++ app.js
@@ -2,7 +2,6 @@
 var path = require('path'),
 	config = require(path.resolve(__dirname, 'api', 'services', 'ConfigManager')).getConfig();
 require('sails').lift({
-  port: config.port,
   prod: config.production,
   hooks: {
     sockets: false,
